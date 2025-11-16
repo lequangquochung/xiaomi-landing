aa02d26930824179ad1373af41189d8734fe0cf9
hunglqq <lequangquochung1912@gmail.com>
2025-11-17 03:41:08 +0700

    feat: complete to test_0341

--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -21,7 +21,7 @@ export default function Popup({
 
     return (
         <div
-            className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm"
+            className="fixed inset-0 z-999 flex items-center justify-center backdrop-blur-sm"
             aria-modal="true"
             role="dialog"
         >
